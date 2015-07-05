e905f149371f0b88afb81ec1dae8c46139f37c6a
ExoryREPO <drago.enrico@hotmail.it>
2015-07-05 09:18:20 +0200

    NabbCleanser: 1 TAB equals 4 Spaces.

--- NabbCleanser/NabbCleanser.cs
+++ NabbCleanser/NabbCleanser.cs
@@ -38,14 +38,14 @@
                 Menu.AddItem(new MenuItem("use.cleansers", "Use Cleansers.").SetValue(true));
                 Menu.AddItem(new MenuItem("use.cleansevsignite", "Cleanse enemy Ignite.").SetValue(true));
                 Menu.AddItem(new MenuItem("use.info1", "Cleansers = QSS, Dervish, Mercurial, Mikaels"));
-				Menu.AddItem(new MenuItem("use.separator1", ""));
-				Menu.AddItem(new MenuItem("panic_key_enable", "Only Cleanse when pressed button enable").SetValue(true));
-				Menu.AddItem(new MenuItem("use.panic_key", "Only Cleanse when pressed button").SetValue(new KeyBind(32, KeyBindType.Press)));
-				/*
-				Menu.AddItem(new MenuItem("use.separator2", ""));
-				Menu.AddItem(new MenuItem("use.cleansers.second.priority", "Use Cleansers for second-priority ultimates").SetValue(false));
-				Menu.AddItem(new MenuItem("use.info2", "Second Priority Utimates = Nocturne and Twisted Fate's Vision Marks."));
-				*/
+                Menu.AddItem(new MenuItem("use.separator1", ""));
+                Menu.AddItem(new MenuItem("panic_key_enable", "Only Cleanse when pressed button enable").SetValue(true));
+                Menu.AddItem(new MenuItem("use.panic_key", "Only Cleanse when pressed button").SetValue(new KeyBind(32, KeyBindType.Press)));
+                /*
+                Menu.AddItem(new MenuItem("use.separator2", ""));
+                Menu.AddItem(new MenuItem("use.cleansers.second.priority", "Use Cleansers for second-priority ultimates").SetValue(false));
+                Menu.AddItem(new MenuItem("use.info2", "Second Priority Utimates = Nocturne and Twisted Fate's Vision Marks."));
+                */
             }
             Menu.AddItem(new MenuItem("enable", "Enable").SetValue(true));
 
@@ -55,7 +55,7 @@
         bool ShouldUseCleanse()
         {
             return (
-			    ObjectManager.Player.HasBuffOfType(BuffType.Charm)
+                ObjectManager.Player.HasBuffOfType(BuffType.Charm)
              || ObjectManager.Player.HasBuffOfType(BuffType.Fear)
              || ObjectManager.Player.HasBuffOfType(BuffType.Knockup)
              || ObjectManager.Player.HasBuffOfType(BuffType.Polymorph)
@@ -63,92 +63,92 @@
              || ObjectManager.Player.HasBuffOfType(BuffType.Stun)
              || ObjectManager.Player.HasBuffOfType(BuffType.Suppression)
              || ObjectManager.Player.HasBuffOfType(BuffType.Taunt)
-			)
+            )
              && Menu.Item("use.cleanse").GetValue<bool>()
             ; 
         }
 
         bool ShouldUseCleanser()
         {
-			// return "true" if the Player has..
+            // return "true" if the Player has..
             return (
-			    // ..Zeds Target Mark (R)..
-			    ObjectManager.Player.HasBuff("zedulttargetmark")
-				
-			 // ..or Warwicks Suppression (R)..
+                // ..Zeds Target Mark (R)..
+                ObjectManager.Player.HasBuff("zedulttargetmark")
+                
+             // ..or Warwicks Suppression (R)..
              || ObjectManager.Player.HasBuff("InfiniteDuress")
-			 
-			 // ..or Vladimirs Mark (R)..
+             
+             // ..or Vladimirs Mark (R)..
              || ObjectManager.Player.HasBuff("VladimirHemoplague")
-			 
-			 // ..or Mordekaisers Mark (R)..
+             
+             // ..or Mordekaisers Mark (R)..
              || ObjectManager.Player.HasBuff("MordekaiserChildrenOfTheGrave")
-			 
-			 // ..or Poppys Immunity Mark (R)..
+             
+             // ..or Poppys Immunity Mark (R)..
              || ObjectManager.Player.HasBuff("PoppyDiplomaticImmunity")
-			 
-			 // ..or Fizz's Fish Mark (R)..
-			 || ObjectManager.Player.HasBuff("FizzMarinerDoom")
-			)
-			 // ..and the relative option is enabled.
+             
+             // ..or Fizz's Fish Mark (R)..
+             || ObjectManager.Player.HasBuff("FizzMarinerDoom")
+            )
+             // ..and the relative option is enabled.
              && Menu.Item("use.cleansers").GetValue<bool>()
             ; 
         }
-		
-		/*
-		bool ShouldUseSecondPriorityCleanser()
+        
+        /*
+        bool ShouldUseSecondPriorityCleanser()
         {
-			// return "true" if the Player has..
+            // return "true" if the Player has..
             return (
-			    // ..Twisted Fates vision mark (R)..
-			    ObjectManager.Player.HasBuff.StartsWith("Twisted")
-				
-			 // ..Nocturnes R (Fog part)..
+                // ..Twisted Fates vision mark (R)..
+                ObjectManager.Player.HasBuff.StartsWith("Twisted")
+                
+             // ..Nocturnes R (Fog part)..
              || ObjectManager.Player.HasBuff.StartsWith("Nocturne")
-			)
-			 // ..and the relative option is enabled.
+            )
+             // ..and the relative option is enabled.
              && Menu.Item("use.cleansers.second.priority").GetValue<bool>()
             ; 
         }
-		*/
+        */
         
         bool CanAndShouldCleanseIfIgnited()
         {
-			// return "true" if..
+            // return "true" if..
             return 
-			    // ..the player is ignited..
-				ObjectManager.Player.HasBuff("summonerdot")
-				
-			 // ..and the relative option is enabled.
+                // ..the player is ignited..
+                ObjectManager.Player.HasBuff("summonerdot")
+                
+             // ..and the relative option is enabled.
              && Menu.Item("use.cleansevsignite").GetValue<bool>()
             ;
         }
         
         private void UseCleanser()
         {
-			// if the player has QSS and is able to use it..
+            // if the player has QSS and is able to use it..
             if (Items.HasItem(QSS) && Items.CanUseItem(QSS))
-			
-				// ..JUST (DO)USE IT!
-			    Items.UseItem(QSS);
-			
+            
+                // ..JUST (DO)USE IT!
+                Items.UseItem(QSS);
+            
             else if (Items.HasItem(Dervish) && Items.CanUseItem(Dervish)) 
-			
-				// ..JUST (DO)USE IT!
-			    Items.UseItem(Dervish);
-				
-			// else if the player has Mikaels and is able to use it..
+            
+                // ..JUST (DO)USE IT!
+                Items.UseItem(Dervish);
+                
+            // else if the player has Mikaels and is able to use it..
             else if (Items.HasItem(Mikaels) && Items.CanUseItem(Mikaels)) 
-			
-				// ..JUST (DO)USE IT!
-			    Items.UseItem(Mikaels);
-				
-			// else if the player has Mercurial Blade and is able to use it..
+            
+                // ..JUST (DO)USE IT!
+                Items.UseItem(Mikaels);
+                
+            // else if the player has Mercurial Blade and is able to use it..
             else if (Items.HasItem(Mercurial) && Items.CanUseItem(Mercurial))
-			
-				// ..JUST (DO)USE IT!
-			    Items.UseItem(Mercurial)
-			;
+            
+                // ..JUST (DO)USE IT!
+                Items.UseItem(Mercurial)
+            ;
         }
         
         /// <summary>
@@ -157,49 +157,49 @@
         /// <param name="args">The <see cref="EventArgs" /> instance containing the event data.</param>
         private void Game_OnGameUpdate(EventArgs args)
         {
-			// Don't use the assembly if the relative option is not enabled.
+            // Don't use the assembly if the relative option is not enabled.
             if (!Menu.Item("enable").GetValue<bool>()) return;
-			
-			// If the only-cleanse-if-key-pressed option is enabled and the relative key is being pressed or the only-cleanse-if-key-pressed option is disabled..
+            
+            // If the only-cleanse-if-key-pressed option is enabled and the relative key is being pressed or the only-cleanse-if-key-pressed option is disabled..
             if ((Menu.Item("panic_key_enable").GetValue<bool>() && Menu.Item("use.panic_key").GetValue<KeyBind>().Active) || (!Menu.Item("panic_key_enable").GetValue<bool>())){
-			
-				cleanse = ObjectManager.Player.GetSpellSlot("summonerboost");
-				
-				// If you are being affected by movement-empairing or control-denying cctype or you are being affected by summoner Ignite..
-				if (ShouldUseCleanse() || CanAndShouldCleanseIfIgnited())
-				{
-					var IsCleanseReady = ObjectManager.Player.Spellbook.CanUseSpell(cleanse) == SpellState.Ready;
-					var HasZedTargetMark = ObjectManager.Player.HasBuff("zedulttargetmark");
-					
-					// If the player actually has the summonerspell Cleanse and it is ready to use..
-					if (cleanse != SpellSlot.Unknown && IsCleanseReady)
-					
-						// If the player is being affected by the DeathMark..
-						if (HasZedTargetMark)
-							
-							// ..Cleanse it, but delay the action by 4 seconds.
-						    Utility.DelayAction.Add(4000, () => ObjectManager.Player.Spellbook.CastSpell(cleanse, ObjectManager.Player));
-							
-						// ..else..	
-						else
-							// ..JUST (DO)CLEANSE IT!
-							ObjectManager.Player.Spellbook.CastSpell(cleanse, ObjectManager.Player)
-						;
-						
-					// ..else..	
-					else
-						// ..try to use a cleanser item.
-						UseCleanser()
-					;
-				}
-				
-				// If the player is being affected by Hard CC or a Second-priority ult mark..
-				if (ShouldUseCleanser() /*|| ShouldUseSecondPriorityCleanser()*/)
-				{
-					// ..JUST (DO)CLEANSE IT!
-					UseCleanser();
-				}
-			}	
+            
+                cleanse = ObjectManager.Player.GetSpellSlot("summonerboost");
+                
+                // If you are being affected by movement-empairing or control-denying cctype or you are being affected by summoner Ignite..
+                if (ShouldUseCleanse() || CanAndShouldCleanseIfIgnited())
+                {
+                    var IsCleanseReady = ObjectManager.Player.Spellbook.CanUseSpell(cleanse) == SpellState.Ready;
+                    var HasZedTargetMark = ObjectManager.Player.HasBuff("zedulttargetmark");
+                    
+                    // If the player actually has the summonerspell Cleanse and it is ready to use..
+                    if (cleanse != SpellSlot.Unknown && IsCleanseReady)
+                    
+                        // If the player is being affected by the DeathMark..
+                        if (HasZedTargetMark)
+                            
+                            // ..Cleanse it, but delay the action by 4 seconds.
+                            Utility.DelayAction.Add(4000, () => ObjectManager.Player.Spellbook.CastSpell(cleanse, ObjectManager.Player));
+                            
+                        // ..else..    
+                        else
+                            // ..JUST (DO)CLEANSE IT!
+                            ObjectManager.Player.Spellbook.CastSpell(cleanse, ObjectManager.Player)
+                        ;
+                        
+                    // ..else..    
+                    else
+                        // ..try to use a cleanser item.
+                        UseCleanser()
+                    ;
+                }
+                
+                // If the player is being affected by Hard CC or a Second-priority ult mark..
+                if (ShouldUseCleanser() /*|| ShouldUseSecondPriorityCleanser()*/)
+                {
+                    // ..JUST (DO)CLEANSE IT!
+                    UseCleanser();
+                }
+            }    
         }
     }
 }
